4e9c8c6ec057c4f80a827ff5d8ecea4d5da335dc
James L <59751464+Jazz1016@users.noreply.github.com>
2021-08-25 11:55:00 -0600

    first commit

---
+++ UnitTesting/UnitTesting/MathStuff.swift
@@ -0,0 +1,28 @@
+//
+//  MathStuff.swift
+//  UnitTesting
+//
+//  Created by James Lea on 8/25/21.
+//
+
+import Foundation
+
+class MathStuff {
+    
+    func addNumbers(x: Int, y: Int) -> Int {
+        return x + y
+    }
+    
+    func subtractNumbers(x: Int, y: Int) -> Int {
+        return x - y
+    }
+    
+    func multipleNumbers(x: Int, y: Int) -> Int {
+        return x * y
+    }
+    
+    func divideNumbers(x: Int, y: Int) -> Int {
+        return x / y
+    }
+    
+}//End of class
---
+++ UnitTesting/UnitTestingTests/UnitTestingTests.swift
@@ -0,0 +1,21 @@
+//
+//  UnitTestingTests.swift
+//  UnitTestingTests
+//
+//  Created by James Lea on 8/25/21.
+//
+
+import XCTest
+@testable import UnitTesting
+
+class UnitTestingTests: XCTestCase {
+    
+    func testAddStuff() {
+        
+        let math = MathStuff()
+        
+        
+        
+    }
+
+}
